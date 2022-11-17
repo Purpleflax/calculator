735485d202fb40854438bdc3f8ab8f5360696ad0
Jeffrey Reynolds <reynoldsjeffrey414@gmail.com>
2022-11-17 12:01:39 +0000

    Adds base html calculator format and general operator function

--- calculator.js
+++ calculator.js
@@ -1,12 +1,34 @@
+function operate(firstNum, operator, secondNum) {
+    let result = 0;
+    switch(operator) {
+        case "+":
+            result = addNum(firstNum, secondNum);
+            break;
+        case "-":
+            result = subtractNum(firstNum, secondNum);
+            break;
+        case "/":
+            result = divideNum(firstNum, secondNum);
+            break;
+        case "*":
+            result = multiplyNum(firstNum, secondNum);
+            break;
+        default:
+            result = addNum(firstNum, secondNum);
+            break;
+    }
+    return(result);
+}
+
 function addNum(numOne, numTwo) {
-    return(parseInt(numOne + numTwo));
+    return(parseInt(numOne) + parseInt(numTwo));
 }
 function subtractNum(numOne, numTwo) {
-    return(parseInt(numOne - numTwo));
+    return(parseInt(numOne) - parseInt(numTwo));
 }
 function divideNum(numOne, numTwo) {
-    return(parseInt(numOne / numTwo));
+    return(parseInt(numOne) / parseInt(numTwo));
 }
 function multiplyNum(numOne, numTwo) {
-    return(parseInt(numOne * numTwo));
+    return(parseInt(numOne) * parseInt(numTwo));
 }
