00db4cadb301425f3b4ca56dcaf811e2413bc5a2
Jeffrey Reynolds <reynoldsjeffrey414@gmail.com>
2022-11-17 11:20:08 +0000

    Adds basic operator functions

---
+++ calculator.js
@@ -0,0 +1,12 @@
+function addNum(numOne, numTwo) {
+    return(parseInt(numOne + numTwo));
+}
+function subtractNum(numOne, numTwo) {
+    return(parseInt(numOne - numTwo));
+}
+function divideNum(numOne, numTwo) {
+    return(parseInt(numOne / numTwo));
+}
+function multiplyNum(numOne, numTwo) {
+    return(parseInt(numOne * numTwo));
+}
